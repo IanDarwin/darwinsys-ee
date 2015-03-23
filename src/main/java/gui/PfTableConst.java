f32346e3d55b0de5db5f2490cfc575e6fc9ebdcd
Ian Darwin <ian@darwinsys.com>
2015-03-22 20:00:28 -0400

    Make @Singleton with accessors

--- src/main/java/gui/PfTableConst.java
+++ src/main/java/gui/PfTableConst.java
@@ -1,11 +1,13 @@
 package gui;
 
 import javax.inject.Named;
+import javax.ejb.Singleton;
 
 /**
  * Constants for use with PrimeFaces DataTable.
  * Arguably some should be in a resource.properties type file...
  */
+@Singleton
 @Named("pfTableConstants")
 public class PfTableConst {
 
@@ -15,4 +17,16 @@ public class PfTableConst {
 		"{CurrentPageReport}  {FirstPageLink} {PreviousPageLink} {PageLinks} {NextPageLink} {LastPageLink} {RowsPerPageDropdown}";
 
 	final static String ROWS_PER_PAGE_TEMPLATE="5,10,15,25";
+
+	public int getRows() {
+		return ROWS;
+	}
+
+	public String getPaginatorTemplate() {
+		return PAGINATOR_TEMPLATE;
+	}
+
+	public String getRowsPerPageTemplate() {
+		return ROWS_PER_PAGE_TEMPLATE;
+	}
 }
