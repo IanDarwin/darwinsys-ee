1f3ebe4a1ce863a2bd1a76589442a36501b5bc33
Ian Darwin <ian@darwinsys.com>
2017-06-30 13:02:01 -0400

    Simplify EntityConverter to be in-memory, hoards objects so bwr heapsize

--- src/main/java/action/EntityConverter.java
+++ src/main/java/action/EntityConverter.java
@@ -1,121 +1,46 @@
 package action;
 
-import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.UUID;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
-import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.Id;
 
 /**
- * The Missing Generic JPA Entity Converter for JSF2
- * Note: Assumes that the given entity has a pk of type Long
- * @author Originally based on incomplete code at
- * http://good-helper.blogspot.ca/2013/10/generic-entity-converter-in-jsf-2-and.html
+ * In-memory JSF generic EntityConverter
+ * @author "Makky" in a comment on http://good-helper.blogspot.ca/2013/10/generic-entity-converter-in-jsf-2-and.html
+ * Warning: will explode memory if used on too many large objects!
  */
-@FacesConverter("entityConverter")
-public class EntityConverter implements Converter, Serializable {
+@FacesConverter(value = "entityConverter")
+public class EntityConverter implements Converter {
 
-	private static final long serialVersionUID = 6596576590941959208L;
-	@Inject
-	private EntityManager em;
-	
-	public EntityConverter() {
-		// Required empty constructor
-	}
+	private static Map<Object,String> entities = new HashMap<>();
 
-	/**
-	 * "Convert the specified string value, which is associated with the specified UIComponent,
-	 * into a model data object that is appropriate for being stored
-	 * during the Apply Request Values phase of the request 
-	 * processing lifecycle." - EE JavaDoc
-	 * <br>
-	 * IOW, JSF calls us with a string like "package.Entity:123"
-	 * (which we generated in getAsString, below).
-	 * @param fc The FacesContext
-	 * @param component The UI component needing the converted result
-	 * @param clazzAndId The clazzname:pkId string
-	 * @return The re-created object
-	 */
 	@Override
-	public Object getAsObject(
-		FacesContext fc, 
-		UIComponent component,
-		String clazzAndId) {
-
-		System.out.println("EntityConverter.getAsObject(): " + clazzAndId);
-
-		if (clazzAndId == null) {
-			return null;
-		}
-		try {
-			String[] split = clazzAndId.split(":");
-			// System.out.printf(
-			//	"EntityConverter: Using em %s to find %s of id %s\n", 
-			//	em, split[0], split[1]);
-			return em.find(Class.forName(split[0]), Long.valueOf(split[1]));
-		} catch (NumberFormatException | ClassNotFoundException e) {
-			String mesg = "getAsObject() failed for " + clazzAndId + "(" + e + ")";
-			System.out.println(mesg);
-			throw new ConverterException(mesg, e);
+	public String getAsString(FacesContext context, UIComponent component, Object entity) {
+		synchronized (entities) {
+			if (!entities.containsKey(entity)) {
+				String uuid = UUID.randomUUID().toString();
+				entities.put(entity, uuid);
+				return uuid;
+			} else {
+				return entities.get(entity);
+			}
 		}
 	}
 
-	/**
-	 * Convert the specified model object value, which is 
-	 * associated with the specified UIComponent, into a String 
-	 * that is suitable for being included in the response 
-	 * generated during the Render Response phase of the 
-	 * request processing lifeycle." -- EE JavaDoc
-	 * IOW, JSF calls us with an object, we want to represent it
-	 * as a string like "package.Entity:123"
-	 * @param fc The FacesContext
-	 * @param component The UI component needing the converted result
-	 * @param object The object to be represented as aclazzname:pkId string
-	 * @return The clazzname:pkId string
-	 */
 	@Override
-	public String getAsString(
-		FacesContext fc, 
-		UIComponent component,
-		Object object) {
-		// System.out.println("EntityConverter.getAsString(): " + object);
-		if (object == null) {
-			return "";	// Required by specification
-		}
-		try {
-			Class<? extends Object> clazz = object.getClass();
-			// Look for field annotation
-			for (Field f : clazz.getDeclaredFields()) {
-				if (f.isAnnotationPresent(Id.class)) {
-					f.setAccessible(true);
-					Long id = (Long) f.get(object);
-					return clazz.getCanonicalName() + ":" + id.toString();
-				}
+	public Object getAsObject(FacesContext context, UIComponent component, String uuid) {
+		for (Entry<Object, String> entry : entities.entrySet()) {
+			if (entry.getValue().equals(uuid)) {
+				return entry.getKey();
 			}
-			// Meh. Look for method annotation.
-			for (Method m : clazz.getDeclaredMethods()) {
-				if (m.isAnnotationPresent(Id.class)) {
-					m.setAccessible(true);
-					Long id = (Long) m.invoke(object, new Object[0]);
-					return clazz.getCanonicalName() + ":" + id.toString();
-				}
-			}
-			// Sigh. No joy.
-			final String mesg = "EntityConverter.getAsString(): no Id on " + clazz;
-			System.err.println(mesg);
-			throw new ConverterException(mesg);
-		} catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
-			String mesg = "getAsString failed to convert " + object + " (" + e + ")";
-			System.err.println(mesg);
-			throw new ConverterException(mesg, e);
 		}
+		return null;
 	}
+
 }
