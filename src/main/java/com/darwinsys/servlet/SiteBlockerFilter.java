9aca3bbc007405c8e73c800f0a67f7069f2e2bca
Ian Darwin <ian@darwinsys.com>
2019-01-29 22:07:07 -0500

    Start blocking DOS attacks from a site in .de

---
+++ src/main/java/com/darwinsys/servlet/SiteBlockerFilter.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.darwinsys.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Block one or more sites by DNS; this is to combat DOS attacks.
+ * Initial version is hard-coded PoC; will use context initParameter, plurality
+ */
+// NOT ANNOTATED - enable in web.xml (principle of least surprise)
+public class SiteBlockerFilter implements Filter {
+	
+	final static String BAD_DOMAIN = "your-server.de";
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		String origin = req.getRemoteHost();
+		if (origin.endsWith(BAD_DOMAIN)) {
+			resp.sendError(404, "Something missing here");
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#destroy()
+	 */
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
